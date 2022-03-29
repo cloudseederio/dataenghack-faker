b0933cd55e37a92397c4463010091ec99f95f7ac
Peter Hanssens <phanssens1@gmail.com>
2022-03-29 22:23:38 +1100

    chore: project created with projen

---
+++ .projenrc.py
@@ -0,0 +1,11 @@
+from projen.python import PythonProject
+
+project = PythonProject(
+    author_email="phanssens1@gmail.com",
+    author_name="Peter Hanssens",
+    module_name="dataenghack_faker",
+    name="dataenghack-faker",
+    version="0.1.0",
+)
+
+project.synth()
