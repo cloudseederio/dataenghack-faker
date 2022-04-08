ae5cc9d96cb56dea7779ba6c5f558242c85fd053
Peter Hanssens <phanssens1@gmail.com>
2022-04-09 00:21:44 +1000

    Seed data (#2)
    * generating seed data
    
    * adding some more base data
    
    * more datasources

--- dataenghack_faker/meetup_members.py
+++ dataenghack_faker/meetup_members.py
@@ -3,9 +3,12 @@ import datetime
 import random
 import json
 import random
+import faker
 import pandas as pd
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import string
+
 
 start_date = "2017-08-01"
 
@@ -46,44 +49,54 @@ def random_company(city):
     else:
         return 'Unknown'
 
+
 def stream_write_json(json_object):
-    with open('outputs/meetup_events.json','a') as f:
+    with open('outputs/meetup_members.json','a') as f:
         f.write(json.dumps(json_object))
         f.write("\u000a")
 
-def get_meetup():
+def get_member():
     x = 1
-    while x < 60:
+    while x < 2200:
         for city in cities:
             date_format = '%Y-%m-%d'
             dtObj = datetime.strptime(start_date, date_format)
-            future_date = dtObj + relativedelta(months=x)
+            future_date = dtObj + relativedelta(days=x)
             # print(f'city: {city}')
             city_name = city['city']
             creation_date = datetime.strptime(city['creation_date'], date_format)
             if future_date >= creation_date:
-                day_date = random.randint(1,28)
-                meetup_date = f'{future_date.year}-{future_date.month}-{day_date}'
-                print(f'{city_name} is on {meetup_date}')
-                result = generate_meetup_events(city_name, random_company(city_name), meetup_date, future_date.strftime("%B"))
+                signup_date = f'{future_date.year}-{future_date.month}-{future_date.day}'
+                print(f'{city_name} is on {signup_date}')
+                result = generate_member(city_name, random_company(city_name)[0], signup_date)
                 stream_write_json(result)
         x += 1
 
-def generate_meetup_events(city, company, date, month):
+def generate_member(city, company, date):
     fake = Faker('en_AU')
+    email = fake.email()
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    mobile = fake.phone_number()
+    domain_name = company.split(' ')[0].translate((str.maketrans('', '', string.punctuation))).lower()
+    company_email = f'{first_name}.{last_name}@{domain_name}.com.au'
 
     # a Python object (dict):
-    event_object = {
+    member_object = {
         "city": city,
-        "event_name": f'{city} DataEng meetup, {month} Edition',
-        "date": date,
-        "host_company" : company[0],
+        "signup_date": date,
+        "first_name": first_name,
+        "last_name": last_name,
+        "company" : company,
+        "mobile": mobile,
+        "email": email,
+        "company_email": company_email 
     }
 
-    print(f'company object: {event_object}')
+    print(f'member object: {member_object}')
 
-    return event_object
+    return member_object
     
 if __name__ == '__main__':
-    get_meetup()
+    get_member()
     print("CSV generation complete!")
